98d3f58e6a8b43a4ee9441ca52019fac3bf9a531
Matías <matiasarce214@gmail.com>
2026-06-19 14:14:03 -0300

    Creando primer endpoint

---
+++ Dsw2026Ej15/Dsw2026Ej15.Api/Controllers/DoctorController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Dsw2026Ej15.Api.Models;
+using Dsw2026Ej15.Domain.Interfaces;
+using Dsw2026Ej15.Domain.Entities;
+
+namespace Dsw2026Ej15.Api.Controllers;
+
+[ApiController]
+public class DoctorController : AppController
+{
+    private readonly IPersistence _persistence;
+    public DoctorController(IPersistence persistence)
+    {
+        _persistence = persistence;
+    }
+
+    [HttpPost("doctors")]
+    public async Task<IActionResult> CreateDoctor(DoctorModel.Request request)
+    {
+        if(string.IsNullOrWhiteSpace(request.Name) || string.IsNullOrWhiteSpace(request.LicenseNumber))
+        {
+            return BadRequest("Nombre y matrícula son requeridos");
+        }
+
+        var speciality = _persistence.GetSpecialityById(request.SpecialityId);
+
+        if(speciality is null)
+        {
+            return BadRequest("Especialidad no existe");
+        }
+
+        var doctor = new Doctor(request.Name, request.LicenseNumber, speciality);
+        _persistence.SaveDoctor(doctor);
+
+        return Created();
+    }
+}
